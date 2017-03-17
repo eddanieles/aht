43783f8ab1cd88b378339e68e865a337b34de84b
eddanieles <ed.danieles@yahoo.com>
2017-03-17 02:04:31 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from './components/header';
 import Questions from './components/questions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Questions />
       </div>
     );
